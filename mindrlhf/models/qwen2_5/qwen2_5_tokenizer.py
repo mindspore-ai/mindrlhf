fed46d48a42597fcdb40f2aab8ce959536c4382d
yang guodong <yangguodong24@huawei.com>
2024-12-18 15:53:19 +0800

    fix setup

--- mindrlhf/models/qwen2_5/qwen2_5_tokenizer.py
+++ mindrlhf/models/qwen2_5/qwen2_5_tokenizer.py
@@ -28,6 +28,7 @@ from mindformers.models.tokenization_utils import PreTrainedTokenizer
 from mindformers.models.tokenization_utils_base import AddedToken
 from mindformers.tools.utils import check_file
 
+__all__ = ['Qwen2_5Tokenizer']
 
 VOCAB_FILES_NAMES = {
     "vocab_file": "vocab.json",
