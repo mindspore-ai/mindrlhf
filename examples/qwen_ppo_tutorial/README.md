a69593932d00d61446579e62786e49d903377b9a
yuzhiwei <yuzhiwei13@huawei.com>
2025-01-14 09:54:02 +0800

    standard code format

--- examples/qwen_ppo_tutorial/README.md
+++ examples/qwen_ppo_tutorial/README.md
@@ -17,36 +17,36 @@
 ```sh
 cd examples/qwen_ppo_tutorial
 python rlhf_data.py \
-	--vocab_path path/to/vocab.json \
-	--merges_file_path path/to/merges.txt \
-	--file_path path/to/input.jsonl \
-	--output_path path/to/output.mindrecord \
-	--max_prompt_length 4096 \
-	--seq_length 8193
+ --vocab_path path/to/vocab.json \
+ --merges_file_path path/to/merges.txt \
+ --file_path path/to/input.jsonl \
+ --output_path path/to/output.mindrecord \
+ --max_prompt_length 4096 \
+ --seq_length 8193
 ```
 
 ## 运行命令
 
 ```sh
 msrun --worker_num=4 --local_worker_num=4 \
-	--master_addr=127.0.0.1 --master_port=9123 \
-	--join=True --log_dir=./qwen2_one_log \
-	examples/qwen_ppo_tutorial/ppo_one_stage.py \
-	--sft_path_infer ./model_configs/qwen_ppo/generate_qwen2_7b.yaml \
-	--sft_path_train ./model_configs/qwen_ppo/finetune_qwen2_7b.yaml \
-	--reward_path ./model_configs/qwen_ppo/generate_qwen2_7b.yaml \
-	--critic_path ./model_configs/qwen_ppo/generate_qwen2_7b.yaml \
-	--mind_dataset_dir path/to/output.mindrecord \
-	--save_ckpt_dir path/to/save_ckpt \
-	--use_parallel True \
-	--load_sft_checkpoint_infer path/to/infer_ckpt \
-	--load_sft_checkpoint_train path/to/train_ckpt \
-	--enable_compile_cache False \
-	--only_save_strategy False
+ --master_addr=127.0.0.1 --master_port=9123 \
+ --join=True --log_dir=./qwen2_one_log \
+ examples/qwen_ppo_tutorial/ppo_one_stage.py \
+ --sft_path_infer ./model_configs/qwen_ppo/generate_qwen2_7b.yaml \
+ --sft_path_train ./model_configs/qwen_ppo/finetune_qwen2_7b.yaml \
+ --reward_path ./model_configs/qwen_ppo/generate_qwen2_7b.yaml \
+ --critic_path ./model_configs/qwen_ppo/generate_qwen2_7b.yaml \
+ --mind_dataset_dir path/to/output.mindrecord \
+ --save_ckpt_dir path/to/save_ckpt \
+ --use_parallel True \
+ --load_sft_checkpoint_infer path/to/infer_ckpt \
+ --load_sft_checkpoint_train path/to/train_ckpt \
+ --enable_compile_cache False \
+ --only_save_strategy False
 
 # 参数说明
 # only_save_strategy: 若要获取各模型的分布式策略文件，设置为True。程序将编译模型，得到策略文件并保存到 ./strategy 目录，然后直接退出。
 ```
 
-​	PPOTrainer中有ppo_model_infer和ppo_model_train两个成员变量，两者分布式策略不同，加载不同的yaml进行初始化。
+​    PPOTrainer中有ppo_model_infer和ppo_model_train两个成员变量，两者分布式策略不同，加载不同的yaml进行初始化。
 
--- mindrlhf/models/base_model.py
+++ mindrlhf/models/base_model.py
@@ -25,12 +25,19 @@ from mindrlhf.models.baichuan2.baichuan2_7b import Baichuan7BV2ForCausalLM
 
 
 class BaseModel(nn.Cell):
-    '''BaseModel'''
-    _model_list = ['pangu', 'bloom', 'baichuan2_7b', 'baichuan2_13b', 'gpt2', 'llama',
-                   'glm4']
+    """BaseModel"""
 
     def __init__(self):
         super(BaseModel, self).__init__()
+        self._model_list = [
+            "pangu",
+            "bloom",
+            "baichuan2_7b",
+            "baichuan2_13b",
+            "gpt2",
+            "llama",
+            "glm4",
+        ]
 
     def select_actor_model(self, model_config):
         """
@@ -43,32 +50,34 @@ class BaseModel(nn.Cell):
             if model in model_config.model_name:
                 self.model_type = model
         if not self.model_type:
-            raise NotImplementedError("only support {}".format(' '.join(self._model_list)))
-        if self.model_type == 'pangu':
+            raise NotImplementedError(
+                "only support {}".format(" ".join(self._model_list))
+            )
+        if self.model_type == "pangu":
             self.model = PanguAlphaHeadModel(model_config)
             self.backbone = self.model.backbone
             self.lm_head = self.model.head
-        elif self.model_type == 'bloom':
+        elif self.model_type == "bloom":
             self.model = BloomLMHeadModel(model_config)
             self.backbone = self.model.transformer
             self.lm_head = self.model.head
-        elif self.model_type == 'baichuan2_7b':
+        elif self.model_type == "baichuan2_7b":
             self.model = Baichuan7BV2ForCausalLM(model_config)
             self.backbone = self.model.model
             self.lm_head = self.model.lm_head
-        elif self.model_type == 'baichuan2_13b':
+        elif self.model_type == "baichuan2_13b":
             self.model = Baichuan13BV2ForCausalLM(model_config)
             self.backbone = self.model.model
             self.lm_head = self.model.lm_head
-        elif self.model_type == 'gpt2':
+        elif self.model_type == "gpt2":
             self.model = GPT2LMHeadModel(model_config)
             self.backbone = self.model.backbone
             self.lm_head = self.model.head
-        elif self.model_type == 'llama':
+        elif self.model_type == "llama":
             self.model = LlamaForCausalLM(model_config)
             self.backbone = self.model.model
             self.lm_head = self.model.lm_head
-        elif self.model_type == 'glm4':
+        elif self.model_type == "glm4":
             self.model = ChatGLM2ForConditionalGeneration(model_config)
             self.backbone = self.model.transformer
             self.lm_head = self.model.transformer.output_layer
@@ -84,26 +93,28 @@ class BaseModel(nn.Cell):
             if model in model_config.model_name:
                 self.model_type = model
         if not self.model_type:
-            raise NotImplementedError("only support {}".format(' '.join(self._model_list)))
-        if self.model_type == 'pangu':
+            raise NotImplementedError(
+                "only support {}".format(" ".join(self._model_list))
+            )
+        if self.model_type == "pangu":
             self.model = PanguAlphaHeadModel(model_config)
             self.backbone = self.model.backbone
-        elif self.model_type == 'bloom':
+        elif self.model_type == "bloom":
             self.model = BloomLMHeadModel(model_config)
             self.backbone = self.model.transformer
-        elif self.model_type == 'baichuan2_7b':
+        elif self.model_type == "baichuan2_7b":
             self.model = Baichuan7BV2ForCausalLM(model_config)
             self.backbone = self.model.model
-        elif self.model_type == 'baichuan2_7b':
+        elif self.model_type == "baichuan2_7b":
             self.model = Baichuan13BV2ForCausalLM(model_config)
             self.backbone = self.model.model
-        elif self.model_type == 'gpt2':
+        elif self.model_type == "gpt2":
             self.model = GPT2LMHeadModel(model_config)
             self.backbone = self.model.backbone
-        elif self.model_type == 'llama':
+        elif self.model_type == "llama":
             self.model = LlamaForCausalLM(model_config)
             self.backbone = self.model.model
-        elif self.model_type == 'glm4':
+        elif self.model_type == "glm4":
             self.model = ChatGLM2ForConditionalGeneration(model_config)
             self.backbone = self.model.transformer
 
@@ -118,25 +129,27 @@ class BaseModel(nn.Cell):
             if model in model_config.model_name:
                 self.model_type = model
         if not self.model_type:
-            raise NotImplementedError("only support {}".format(' '.join(self._model_list)))
-        if self.model_type == 'pangu':
+            raise NotImplementedError(
+                "only support {}".format(" ".join(self._model_list))
+            )
+        if self.model_type == "pangu":
             self.model = PanguAlphaHeadModel(model_config)
             self.backbone = self.model.backbone
-        elif self.model_type == 'bloom':
+        elif self.model_type == "bloom":
             self.model = BloomLMHeadModel(model_config)
             self.backbone = self.model.transformer
-        elif self.model_type == 'baichuan2_7b':
+        elif self.model_type == "baichuan2_7b":
             self.model = Baichuan7BV2ForCausalLM(model_config)
             self.backbone = self.model.model
-        elif self.model_type == 'baichuan2_13b':
+        elif self.model_type == "baichuan2_13b":
             self.model = Baichuan13BV2ForCausalLM(model_config)
             self.backbone = self.model.model
-        elif self.model_type == 'gpt2':
+        elif self.model_type == "gpt2":
             self.model = GPT2LMHeadModel(model_config)
             self.backbone = self.model.backbone
-        elif self.model_type == 'llama':
+        elif self.model_type == "llama":
             self.model = LlamaForCausalLM(model_config)
             self.backbone = self.model.model
-        elif self.model_type == 'glm4':
+        elif self.model_type == "glm4":
             self.model = ChatGLM2ForConditionalGeneration(model_config)
             self.backbone = self.model.transformer
