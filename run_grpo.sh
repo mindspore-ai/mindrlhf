f4f181f94584d181af911944b962f311606b5950
18635393033 <yueliangwei@huawei.com>
2025-03-13 09:12:04 +0800

    fix run_grpo wrong path

--- run_grpo.sh
+++ run_grpo.sh
@@ -13,9 +13,9 @@ export PYTHONPATH=/path/to/mindformers:$PYTHONPATH
 
 msrun --worker_num=8 --local_worker_num=8 --master_addr=127.0.0.1 \
 --master_port=9190 --join=False --log_dir=./qwen2_one_log \
-examples/qwen_grpo_tutorial/grpo_one_stage.py \
---sft_path_infer ./model_configs/qwen_grpo/predict_qwen2_7b_instruct.yaml \
---sft_path_train ./model_configs/qwen_grpo/finetune_qwen2_7b.yaml \
+examples/grpo/qwen_grpo_tutorial/grpo_one_stage.py \
+--sft_path_infer ./model_configs/qwen_grpo/predict_qwen2_5_7b_instruct.yaml \
+--sft_path_train ./model_configs/qwen_grpo/finetune_qwen2_5_7b.yaml \
 --vocab_path /path/to/vocab.json \
 --merges_file_path /path/to/merges.txt \
 --mind_dataset_dir /path/to/grpo_data/cvalues_one.mindrecord \
@@ -25,4 +25,4 @@ examples/qwen_grpo_tutorial/grpo_one_stage.py \
 --load_sft_checkpoint_infer "/path/to/ckpt/infer" \
 --load_sft_checkpoint_train "/path/to/ckpt/train" \
 --load_ref_checkpoint "/path/to/ckpt/infer" \
---enable_compile_cache False \
+--enable_compile_cache False
