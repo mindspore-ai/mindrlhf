11b82be944f14765fdd1c25e9f52a3ca16862b57
云骑士 <yuzhiwei13@huawei.com>
2025-05-08 20:43:18 +0800

    add vllm cp system test

--- tests/st/qwen2_5_vllm/__init__.py
+++ tests/st/qwen2_5_vllm/__init__.py
@@ -12,24 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
-""" test Qwen GRPO training without vllm """
-import os
-import pytest
-
-from tests.st.utils import check_log
-
-root_path = os.path.dirname(os.path.abspath(__file__))
-
-
-@pytest.mark.level0
-@pytest.mark.platform_arm_ascend910b_training
-@pytest.mark.env_single
-@pytest.mark.skip(reason="remove no vllm testcase from ci, only use for local debug")
-def test_qwen_grpo():
-    """ test Qwen GRPO training """
-    os.system(f"bash {root_path}/run_qwen_grpo_test.sh")
-
-    log_path = f"{root_path}/qwen2_one_log/worker_0.log"
-    check_pair = {"Save checkpoints in": 1}
-
-    check_log(log_path, check_pair)
+"""test_st"""
